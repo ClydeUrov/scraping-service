7e4dfe0a15a4f7617d04a5c4aba7772d0078b026
ClydeUrov <clydeurov@gmail.com>
2023-02-07 11:25:14 +0200

    Added models Language and City

--- src/scraping/models.py
+++ src/scraping/models.py
@@ -1,6 +1,23 @@
 from django.db import models
 
 class City(models.Model):
-    name = models.CharField(max_length=50)
-    slug = models.CharField(max_length=50, blank=True)
+    name = models.CharField(max_length=50, verbose_name='Название неселённого пункта')
+    slug = models.CharField(max_length=50, blank=True, unique=True)
 
+    class Meta:
+        verbose_name='Название неселённого пункта'
+        verbose_name_plural = 'Название неселённых пунктов'
+
+    def __str__(self):
+        return self.name
+
+class Language(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Язык программирования')
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+
+    class Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = 'Языки программирования'
+
+    def __str__(self):
+        return self.name
